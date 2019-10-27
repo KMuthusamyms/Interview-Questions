9bb2a8e219dc1a2daec9249da6e30438f2f58f8e
venkatasaikarthikeya <34571463+venkatasaikarthikeya@users.noreply.github.com>
2019-10-27 10:21:33 +0530

    Update insertionSort.c

--- Sorting Algorithms/insertionSort.c
+++ Sorting Algorithms/insertionSort.c
@@ -1,6 +1,11 @@
 /* 
 Time Complexity : O(n^2)
 Space Complexity: O(1)
+for sorting n elements we require n-1 comparisions and n-1 movments, so we might think that by using binary search we can reduce the 
+number of comparisions to log n but number of movments still remain O(n), we might also think that by using doubly linked list we
+can reduce the number of movments to O(1) but number of comparisions remains same in this case. So, either by using binary search or
+by using doubly linked list we might ntot be able to reduce the time complexity.So, the time complexity is O(n^2).
+Inplace algorithm as it does not require any extra space
 Stable sorting algorithm as it preserves the relative ordering of the elements
 */
 #include<stdio.h>
